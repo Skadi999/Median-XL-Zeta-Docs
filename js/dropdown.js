d4b290575323668e9b265bc214a2f344653bf0fd
Skadi999 <vj.novak9@gmail.com>
2020-08-11 10:32:57 +0200

    dropdowns update

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,19 +1,79 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction(a) {
+function clickConcepts() {
   document.querySelector('#conceptDropdown').classList.toggle('show');
   unShowDropDown('characterDropdown');
+  unShowDropDown('itemDropdown');
+  unShowDropDown('uberDropdown');
+  unShowDropDown('challengeDropdown');
+  unShowDropDown('mapDropdown');
+  unShowDropDown('miscDropdown');
 }
 
-function myFunctionTwo(a) {
-  document.querySelector('#characterDropdown').classList.toggle('show')
+function clickCharacters() {
+  document.querySelector('#characterDropdown').classList.toggle('show');
   unShowDropDown('conceptDropdown');
+  unShowDropDown('itemDropdown');
+  unShowDropDown('uberDropdown');
+  unShowDropDown('challengeDropdown');
+  unShowDropDown('mapDropdown');
+  unShowDropDown('miscDropdown');
+}
+
+function clickItems() {
+  document.querySelector('#itemDropdown').classList.toggle('show');
+  unShowDropDown('conceptDropdown');
+  unShowDropDown('characterDropdown');
+  unShowDropDown('uberDropdown');
+  unShowDropDown('challengeDropdown');
+  unShowDropDown('mapDropdown');
+  unShowDropDown('miscDropdown');
+}
+
+function clickUbers() {
+  document.querySelector('#uberDropdown').classList.toggle('show');
+  unShowDropDown('conceptDropdown');
+  unShowDropDown('characterDropdown');
+  unShowDropDown('itemDropdown');
+  unShowDropDown('challengeDropdown');
+  unShowDropDown('mapDropdown');
+  unShowDropDown('miscDropdown');
+}
+
+function clickChallenges() {
+  document.querySelector('#challengeDropdown').classList.toggle('show');
+  unShowDropDown('conceptDropdown');
+  unShowDropDown('characterDropdown');
+  unShowDropDown('itemDropdown');
+  unShowDropDown('uberDropdown');
+  unShowDropDown('mapDropdown');
+  unShowDropDown('miscDropdown');
+}
+
+function clickMaps() {
+  document.querySelector('#mapDropdown').classList.toggle('show');
+  unShowDropDown('conceptDropdown');
+  unShowDropDown('characterDropdown');
+  unShowDropDown('itemDropdown');
+  unShowDropDown('uberDropdown');
+  unShowDropDown('challengeDropdown');
+  unShowDropDown('miscDropdown');
+}
+
+function clickMisc() {
+  document.querySelector('#miscDropdown').classList.toggle('show');
+  unShowDropDown('conceptDropdown');
+  unShowDropDown('characterDropdown');
+  unShowDropDown('itemDropdown');
+  unShowDropDown('uberDropdown');
+  unShowDropDown('challengeDropdown');
+  unShowDropDown('mapDropdown');
 }
 
 function unShowDropDown(dropDownID) {
   var otherDropDown = document.querySelector(`#${dropDownID}`);
-  otherDDEnabled = otherDropDown.classList.contains('show')
-  if(otherDDEnabled) otherDropDown.classList.toggle('show')
+  otherDDEnabled = otherDropDown.classList.contains('show');
+  if(otherDDEnabled) otherDropDown.classList.toggle('show');
 }
 
 // Close the dropdown menu if the user clicks outside of it
