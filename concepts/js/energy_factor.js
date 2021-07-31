b9bc881284cbab0d1f70b52271c598224cc9ee06
Vyacheslav Novak <vj.novak9@gmail.com>
2021-07-31 13:01:54 +0200

    More fixes

--- concepts/js/energy_factor.js
+++ concepts/js/energy_factor.js
@@ -1,6 +1,6 @@
 function calculateSpellDamage() {
   var energy = parseInt(document.getElementById('energy').value);
   var energyFactor = parseInt(document.getElementById('efactor').value);
-  var result = 260 * energy / (250 + energy) + Math.min((energy * energyFactor / 15000), 200);
+  var result = 260 * (energy + 10) / (250 + energy) + Math.min((energy * energyFactor / 15000), 200);
   document.getElementById('result').innerHTML = 'Your spell damage increase is: ' + result.toFixed(2);
 }
